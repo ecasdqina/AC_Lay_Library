d68921870a9af093c72b9c5d3c8be37b1c067d0f
ecasdqina <e1711@s.akashi.ac.jp>
2019-09-05 13:47:40 +0900

    fix test union_find_tree

--- test/union_find_tree.test.cpp
+++ test/union_find_tree.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
 
-#include "AC_Lay_Library/tree/union_find_tree.cpp"
+#include "../tree/union_find_tree.cpp"
 
 int main() {
 	int n, q; scanf("%d%d", &n, &q);
