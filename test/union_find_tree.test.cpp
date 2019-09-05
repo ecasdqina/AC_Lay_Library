f07630b530bc9e04d559531a128fed9e6b5163ba
ecasdqina <e1711@s.akashi.ac.jp>
2019-09-05 13:23:23 +0900

    test union_find_tree

---
+++ test/union_find_tree.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A
+
+#include "../tree/union_find_tree.cpp"
+
+int main() {
+	int n, q; scanf("%d%d", &n, &q);
+
+	union_find_tree uf(n);
+	while(q--) {
+		int type, x, y; scanf("%d%d%d", &type, &x, &y);
+
+		if(type == 0) uf.unite(x, y);
+		else printf("%d\n", uf.same(x, y));
+	}
+}
