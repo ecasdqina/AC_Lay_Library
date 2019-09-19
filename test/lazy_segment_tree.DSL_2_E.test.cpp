730e99361d3ba7b06254f8daaf04344032639aab
ecasdqina <e1711@s.akashi.ac.jp>
2019-09-19 22:40:30 +0900

    test lazy segtree

---
+++ test/lazy_segment_tree.DSL_2_E.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_D"
+
+#include <stdio.h>
+#include "../tree/lazy_segment_tree.hpp"
+
+using i64 = long long;
+
+int main() {
+	int n, q; scanf("%d%d", &n, &q);
+
+	auto f = [](i64 a, i64 b) { return a + b; };
+	auto g = [](i64 a, i64 b, int l, int r) { return a + b * (i64)(r - l); };
+	auto h = [](i64 a, i64 b) { return a + b; };
+	lazy_segment_tree<int> seg(n, f, g, h, 0, 0);
+	
+	while(q--) {
+		int type; scanf("%d", &type);
+
+		if(type == 0) {
+			int s, t, x; scanf("%d%d%d", &s, &t, &x);
+
+			seg.update(s - 1, t, x);
+		} else if(type == 1) {
+			int i; scanf("%d", &i);
+			
+			printf("%d\n", seg[i - 1]);
+		}
+	}
+
+	return 0;
+}
